ff4481f413eee5a5c299983e57976f17b2a98b87
hasban-fardani <hasbanfardani77@gmail.com>
2023-09-20 18:52:53 +0700

    add meta tag content-security-policy: upgrade-insecure-requests for enable req to backend

--- layouts/pemiros.vue
+++ layouts/pemiros.vue
@@ -9,7 +9,8 @@
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: 'Aplikasi Pemilihan Umum (Pemilu) untuk calon rois roisah IRMA FORMULAS'}
+      { name: 'description', content: 'Aplikasi Pemilihan Umum (Pemilu) untuk calon rois roisah IRMA FORMULAS'},
+      { name: 'content-security-policy', content: 'upgrade-insecure-requests'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'logo irma.png' }
